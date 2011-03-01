ffde4ab13c475ac336aad4f6ba4fcc653e33d573
jean <sjeanc911@gmail.com>
2011-03-01 20:49:56 +0800

    added config files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spacelyf::Application.config.secret_token = '8067fd44abb719f6b10afcabef8671705fd6b8bf51c3aa6215b0f43370e0711715a07030ce065986bbe8336b29a777feedede5dca8e70094ab37d5698970413c'
